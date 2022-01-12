89db48cb2e743cf493cbf0c4234a9876c4001ae8
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2022-01-12 11:30:12 +0530

    Add files via upload

---
+++ SampleConApp-Day1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Namespace is a conceptual grouping of classes to avoid naming conflicts. Namespaces are popular in C++. 
+using System;
+namespace SampleConAppDay1
+{
+    class Program//Name of the class need not be the same as the filename
+    {
+        //Main is the entry point for any C# program. 
+        static void Main()
+        {
+            Console.WriteLine("A String content to be displayed on Console");
+            Console.WriteLine("Another line of code");    
+        }
+    }
+}
+/*
+ * Short cuts in Visual Studio
+ * To build the App: Ctrl + Shift + B.(Process to compile the code and create the EXE for U but will not run) 
+ * To run without debug: Ctrl + F5(Builds the App and executes the App)
+ * To debug the Application: F5. 
+ */
+
