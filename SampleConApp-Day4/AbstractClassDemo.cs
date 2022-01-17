5960057736071aa141f315351a72895f6fd3ab36
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2022-01-17 16:47:54 +0530

    Add files via upload

---
+++ SampleConApp-Day4/AbstractClassDemo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConApp_Day4
+{
+    abstract class Account
+    {
+        public int AccountNo { get; set; }
+        public string HolderName { get; set; }
+        public double Balance { get; private set; }//This property value cannot be set outside the class.
+        public void Credit(double amount) => Balance += amount;
+        public void Debit(double amount)
+        {
+            if (amount > Balance)
+                throw new Exception("Insufficient funds");//Raise an error.
+            Balance -= amount;
+        }
+        public abstract void CalculateInterest();//This method is must for all kinds of accounts but the base class does not have a concrete business rule for implementing it. 
+    }
+
+    class SBAccount : Account
+    {
+        public override void CalculateInterest()
+        {
+            double pricipal = this.Balance;
+            double rateOfInterest = 6.5 / 100;
+            double time = 0.25;
+            double interest = pricipal * rateOfInterest * time;
+            Credit(interest);
+        }
+    }
+
+    class RDAccount : Account
+    {
+        //Formula for calculating the RD for 5 Years at rate of 5.5% with 5000*60
+        public override void CalculateInterest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    internal class AbstractClassDemo
+    {
+        static void Main(string[] args)
+        {
+            Account account = new SBAccount();
+            account.Credit(10000);
+            account.AccountNo = 111;
+            account.HolderName = "Phaniraj";
+            account.CalculateInterest();
+            Console.WriteLine("The Current Balance is {0:C}", account.Balance);
+        }
+    }
+}
---
+++ SampleConApp-Day4/OverridingExample.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace SampleConApp_Day4
+{
+    class FatherClass
+    {
+        public void DisplayBoard() //non-virtual method...
+        {
+            Console.WriteLine("Shopping City, Bangalore".ToUpper());
+        }
+        public virtual void MakePayment(string mode, int amount)
+        {
+            if((mode == "Cash") || (mode == "Cheque"))
+                Console.WriteLine($"The Amount of {amount:C} has been made in the form of {mode}");
+            else
+                Console.WriteLine("This mode of payment is not accepted");
+        }
+    }
+    class SonClass : FatherClass
+    {
+        public new void DisplayBoard()//Consider this as new function and all the rules of Substitution will be applicable
+        {
+            Console.WriteLine("Shopper's City, Bangalore".ToUpper());
+        }
+
+        public override void MakePayment(string mode, int amount)
+        {
+            // base.MakePayment(mode, amount);//calling the base version. base refers to the immediate base class. 
+            if ((mode == "Cash") || (mode == "Card"))
+                Console.WriteLine($"The Amount of {amount:C} has been made in the form of {mode}");
+            else
+                Console.WriteLine("This mode of payment is not accepted");
+        }
+    }
+    /// <summary>
+    /// This class job is to create an instance of any derived class based on the user requirement.
+    /// </summary>
+    class BusinessFactory
+    {
+        public static FatherClass GetBusinessComponent(string name)
+        {
+            if (name.ToUpper() == "FATHER")
+                return new FatherClass();
+            else
+                return new SonClass();
+        }
+    }
+    internal class OverridingExample
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Type of the class U want: Father or Son");
+            string input = Console.ReadLine();
+            FatherClass business = BusinessFactory.GetBusinessComponent(input);//User is not supposed to create the object(Abstraction). Factory is a seperate class that gives the required object based on the user requirement.  
+            if(business is SonClass)
+            {
+                var temp = business as SonClass; //explicit downcasting. 
+                temp.DisplayBoard();
+            }else
+                business.DisplayBoard();//This will not be polymorphic as no method overriding is done 
+            business.MakePayment("Card", 65000);
+            
+        }
+    }
+}
