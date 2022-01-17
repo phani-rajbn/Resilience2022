55261e6d89724ef80b28082008f6dc3c3cfc4f6e
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2022-01-17 12:41:36 +0530

    Add files via upload

---
+++ SampleConApp-Day4/InheritanceDemo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+namespace SampleConApp_Day4
+{
+    //Assumption: The Parent class is created and U  dont have the source code of it, but U have a DLL or a Reference of it. 
+    class ParentClass
+    {
+        public void TestFunc() => Console.WriteLine("Test Func from the base");
+    }
+
+    //Syntax of Inheritance. While inheriting, there is no access specifier for the inheritance
+    class DerivedClass :  ParentClass
+    {
+        public void DerivedFunc() => Console.WriteLine("Derived Func from the Derived");
+    }
+
+    internal class InheritanceDemo
+    {
+        static void Main(string[] args)
+        {
+            ParentClass cls = new ParentClass();
+            cls.TestFunc();
+
+            if (cls is DerivedClass)
+            {
+                var obj = cls as DerivedClass;
+                obj.DerivedFunc();//obj is an alias to the cls where no new object is created but a reference of it is made
+            }
+            else
+            {
+                Console.WriteLine("Cls is not an object of the type Derived class");
+            }
+            //DerivedClass cls2 = new DerivedClass();
+            //cls2.TestFunc();
+            //cls2.DerivedFunc();
+            Console.WriteLine("Now creating the object as Derived type");
+            cls = new DerivedClass();//A base type could be substituted  to its derived type.
+            cls.TestFunc();
+            //New methods of the derived class will not be accessible to the base class object. 
+            ((DerivedClass)cls).DerivedFunc();//This is called as Downcasting, where the base type is casted to its derived type and then used. 
+            //If U want to do downcasting, it is recommended to use is and as operators. is operator is used to check for the instance type and as operator type casted to the type
+            if(cls is DerivedClass)
+            {
+                var obj = cls as DerivedClass;
+                obj.DerivedFunc();//obj is an alias to the cls where no new object is created but a reference of it is made
+            }else
+            {
+                Console.WriteLine("Cls is not an object of the type Derived class");
+            }
+        }
+    }
+}
---
+++ SampleConApp-Day4/Patient.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+/*Create a class called Patient and should have  properties like Id, Name, DateOfbirth, DateOfVisit, DoctorName and Fees. 
+Develop a program to take input from the User for a single patient and display the details  of that patient*/
+namespace SampleConApp_Day4
+{
+    /// <summary>
+    /// Represents a real time patient of a Hospital that store info about outpatient.
+    /// </summary>
+    internal class Patient
+    {
+        public int PatientID { get; set; }
+        public string PatientName { get; set; }
+        public DateTime DateOfBirth{ get; set; }
+        public DateTime DateOfVisit { get; set; } = DateTime.Now;//Default value is current date of the system. 
+        public string DoctorName { get; set; }
+        public double BillAmount { get; set; }
+    }
+
+    /// <summary>
+    /// The User interface class will do all the user interactions like taking inputs and presenting the data to the user.
+    /// </summary>
+    class TestingApp
+    {
+        /// <summary>
+        /// Takes inputs from the user and creates a valid Patient object
+        /// </summary>
+        /// <returns>A Valid Patient object</returns>
+        static Patient createPatient()
+        {
+            Patient patient = new Patient();
+            patient.PatientID = Input.GetNumber("Enter the Patient ID");
+            patient.PatientName = Input.GetAnswer("Enter the Patient Name");
+            patient.DateOfBirth = Input.GetDate("Enter the Date of Birth");
+            patient.BillAmount = Input.GetDouble("Enter the bill Amount");
+            return patient;
+        }
+        static void Main(string[] args)
+        {
+            Patient patient = createPatient();
+            Console.WriteLine($"The patient named {patient.PatientName} information has been stored");
+        }
+    }
+}
