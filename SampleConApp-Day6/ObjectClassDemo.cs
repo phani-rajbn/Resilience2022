aeff6cf531b2269f47bde1e2906934b534f85e9b
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2022-01-19 14:51:12 +0530

    Add files via upload

---
+++ SampleConApp-Day6/ObjectClassDemo.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Object class
+ * E2E Application
+ */
+namespace SampleConApp_Day6
+{
+    //All classes/structs/enums are types derived from System.Object, the super base class of .NET Framework. 
+    class Customer
+    {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public long CustomerPhone { get; set; }
+
+        //U can override the ToString method to provide a Custom conversion to string. 
+        public override string ToString()
+        {
+            string content = $"{CustomerName}-{CustomerPhone}-{CustomerID}";
+            return content;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Customer)//is and as operator works only for reference types...
+            {
+                Customer other =obj as Customer; //UNBOXING....
+                return other.CustomerID == this.CustomerID;
+            } 
+            else return false;
+        }        
+    }
+
+    internal class ObjectClassDemo
+    {
+        static void Main(string[] args)
+        {
+            //object data = intBasedExample();
+            //data = doubleBasedExample();
+            //data = stringBasedExample();
+            //classBasedExample();
+            Customer customer = new Customer { CustomerID = 123, CustomerPhone =9945205684, CustomerName ="Phaniraj" };
+            //Default Implementation of ToString to a class will be its classname            
+            Console.WriteLine(customer);//Displays the Classname of the object if not overriden.
+        //Console.WriteLine always evaluates a value to its string format and displays it. ToString method of the variable will be called internally and then the display happens 
+        //As in this example, we have overriden the ToString method, it will display accordingly..
+
+            Customer customer2 = new Customer { CustomerID = 123, CustomerPhone = 9945205684, CustomerName = "Phaniraj" };
+            Console.WriteLine(customer.Equals(customer2));//Equals is used to evaluate the object equivalence with another passed as arg. Returns true if equal, else false
+        }
+
+        private static void classBasedExample()
+        {
+            //Example of boxing Customer object to the object type. 
+            object obj = new Customer { CustomerID = 123, CustomerName = "Phaniraj", CustomerPhone = 9945205684 };//BOXING..
+            //Customer dataCopy = (Customer)obj;//old style of casting...
+            Customer dataCopy = obj as Customer;
+            Console.WriteLine(dataCopy.CustomerName + " can be contacted on " + dataCopy.CustomerPhone);
+        }
+
+        private static object stringBasedExample()
+        {
+            object data;
+            data = "Sample String";//Hold a string. 
+            Console.WriteLine("The data type of date is " + data.GetType().Name);
+            string strCopy = data.ToString();
+            Console.WriteLine($"To display it in Upper case: {strCopy.ToUpper()}");
+            return data;
+        }
+
+        private static object doubleBasedExample()
+        {
+            object data;
+            data = 123.345;//Hold a double value
+            Console.WriteLine("The data type of date is " + data.GetType().Name);
+            //double copy = (double)data;//rightway
+            //int copy = (int)data;//wrong way as double cannot be converted to int. 
+            //int copy =(int)(double)data; ==>To much confusing...
+            int copy = Convert.ToInt32(data);//Use Convert method instead of old C Style casting. 
+            copy += 123;//Do the operation
+            data = copy;//set it back to the data.
+            return data;
+        }
+
+        private static object intBasedExample()
+        {
+            object data = 123;//data holds the data of the type int.
+            Console.WriteLine("The data type of date is " + data.GetType().Name);
+            return data;
+        }
+    }
+}
---
+++ SampleConApp-Day6/e2eApp.cs
@@ -0,0 +1,201 @@
+﻿
+//Simple Drug Registration Software:
+//Drug: ID, Name, Composition, Price., DateOfManufacture. ExpiryDate(Read Only)
+//Register new Drugs, update Drugs and delete Drugs, GetInfo about drugs. 
+//Menu Driven Program. 
+using SampleConApp_Day6.Entities;
+using System;
+using System.IO;//For file reading....
+using System.Collections.Generic;
+namespace SampleConApp_Day6
+{
+    namespace Entities
+    {
+        class Drug
+        {
+            public override string ToString()
+            {
+                return $"{DrugNo},{Name},{Composition},{Price},{DateOfManufacture}\n";
+            }
+            public int DrugNo { get; set; }
+            public string Name { get; set; }
+            public string Composition { get; set; }
+            public double Price { get; set; }
+            public DateTime DateOfManufacture{ get; set; }
+            public DateTime ExpiryDate { get; private set; }
+            public Drug()
+            {
+                ExpiryDate = DateOfManufacture.AddMonths(6);
+            }
+
+            public void Copy(Drug drg)
+            {
+                DrugNo = drg.DrugNo;
+                Name = drg.Name;
+                Composition = drg.Composition;
+                Price = drg.Price;
+                DateOfManufacture = drg.DateOfManufacture;
+                ExpiryDate = drg.ExpiryDate;
+            }
+        }
+    }
+
+    namespace DataLayer
+    {
+        using Entities;
+        //interface is like a plan, implementation can be done later. 
+        interface IDrugManager
+        {
+            void RegisterNewDrug(Drug drug);
+            void UpdateDrug(Drug drug);
+            Drug[] FindDrug(string name);
+            Drug FindDrug(int drugNo);
+            void DeleteDrug(int id);
+        }
+        class DrugFileManager : IDrugManager
+        {
+            public void DeleteDrug(int id)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Drug[] FindDrug(string name)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Drug FindDrug(int drugNo)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void RegisterNewDrug(Drug drug)
+            {
+                File.AppendAllText("Temp.txt", drug.ToString());
+            }
+
+            public void UpdateDrug(Drug drug)
+            {
+                throw new NotImplementedException();
+            }
+        }
+        class DrugListManager : IDrugManager
+        {
+            private List<Drug> _drugs = new List<Drug>();
+            public void DeleteDrug(int id)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Drug[] FindDrug(string name)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Drug FindDrug(int drugNo)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void RegisterNewDrug(Drug drug)
+            {
+                _drugs.Add(drug);
+            }
+
+            public void UpdateDrug(Drug drug)
+            {
+                throw new NotImplementedException();
+            }
+        }
+        class DrugManager : IDrugManager
+        {
+            private Drug[] _drugs = new Drug[100];//Limitation of the Array, its fixed in size...
+            public void DeleteDrug(int id)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Drug[] FindDrug(string name)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Drug FindDrug(int drugNo)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void RegisterNewDrug(Drug drug)
+            {
+                for(int i =0; i < 100; i++)
+                {
+                    if(_drugs[i] == null)
+                    {
+                        _drugs[i] = new Drug();
+                        _drugs[i].Copy(drug);
+                        return;//Exit the function after the copy is done;
+                    }
+                }
+                throw new Exception("No more drugs to be registered by the System!!!");
+            }
+
+            public void UpdateDrug(Drug drug)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+    /// <summary>
+    /// User interface of the Application
+    /// </summary>
+    internal class e2eApp
+    {
+        private static DataLayer.IDrugManager mgr = new DataLayer.DrugFileManager();//instantiating the object.
+        static void Main(string[] args)
+        {
+            string menu = File.ReadAllText(args[0]);//API of the .NET to get all the text content of a file...
+            bool processing = true;
+            do
+            {
+                processing = processMenu(menu);
+            } while (processing);
+        }
+
+        private static bool processMenu(string menu)
+        {
+            string choice = Input.GetAnswer(menu);
+            switch (choice.ToUpper())
+            {
+                case "N":
+                    addDrugHelper();
+                    return true;
+                case "U":
+                case "F":
+                case "I":
+                case "D":
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        private static void addDrugHelper()
+        {
+            int id = Input.GetNumber("Enter the ID for the Drug");
+            string name = Input.GetAnswer("Enter the name for the drug");
+            string composition = Input.GetAnswer("Enter the Composition seperated by ,");
+            double price = Input.GetDouble("Enter the cost for 10 Units");
+            DateTime doM = Input.GetDate("Enter the Manufacturing date");
+            Drug drg = new Drug { Composition = composition, DateOfManufacture = doM, DrugNo = id,  Name = name, Price = price};
+            try
+            {
+                mgr.RegisterNewDrug(drg);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
