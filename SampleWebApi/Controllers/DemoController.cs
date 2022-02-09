266cf43f2ea8ad2cac892071a0872e05048fa412
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2022-02-09 15:45:20 +0530

    Add files via upload

---
+++ SampleWebApi/Controllers/DemoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;//Namespace for Web API.
+
+namespace SampleWebApi.Controllers
+{
+    public class DemoController : ApiController
+    {
+        [HttpGet]
+        public string[] fruits()
+        {
+            return new string[] { "Apples", "Mangoes", "Bananas", "Oranges", "PineApples", "Kiwis" };
+        }
+
+        [HttpGet]
+        [Route("Cars")]//To define a URL Pattern if U have multiple gets. 
+        public string[] Cars()
+        {
+            return new string[] { "Santro", "Honda WRV", "Kia- Sonnet", "Maruti Zen" };
+        }
+    }
+}
---
+++ SampleWebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using SampleWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SampleWebApi.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public List<EmpTable> GetAllEmployees()
+        {
+            var context = new ResilienceDemoEntities();
+            var empRecords = context.EmpTables.ToList();
+            return empRecords;
+        }
+
+        [HttpPost]
+        public string AddEmployee(EmpTable input)
+        {
+            var context = new ResilienceDemoEntities();
+            context.EmpTables.Add(input);
+            context.SaveChanges();
+            return "Employee added to the database";
+        }
+    }
+}
